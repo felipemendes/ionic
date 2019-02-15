6ef45318c2508808df27fcc0142dafa9f60b2931
felipemendes <f.ribeiromendes@gmail.com>
2019-02-14 23:21:45 -0200

    Fetch events on each tab

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventsService } from '../events.service';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
-
-  constructor() { }
+  event;
+  constructor(private eventService: EventsService) { }
 
   ngOnInit() {
+    this.event = this.eventService.currentEvent;
   }
 
 }
--- src/app/featured/featured.page.ts
+++ src/app/featured/featured.page.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from '../events.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-featured',
   templateUrl: 'featured.page.html',
   styleUrls: ['featured.page.scss']
 })
-export class FeaturedPage {}
+export class FeaturedPage implements OnInit {
+  events: any;
+  constructor(private eventsService: EventsService, private router: Router) {}
+
+  ngOnInit() {
+    this.eventsService
+      .fetchFeed('12fzng')
+      .subscribe(data => {
+        this.events = data;
+      })
+  }
+
+  showDetails(event) {
+    this.eventsService.currentEvent = event;
+    this.router.navigate(['/details']);
+  }
+}
